f905d839201247f9c9c49d102d0e06396e50a00d
TH Huang <tzhsn.huang@gmail.com>
2024-03-19 23:42:46 -0500

    feat: implement time util (#8)

---
+++ util/time/time.go
@@ -0,0 +1,10 @@
+package time
+
+// Go's specific reference date and time: Mon Jan 2 15:04:05 MST 2006
+const (
+	// ParseFormatYYYYMMDDWithHyphen is for formatting dates in the YYYY-MM-DD format.
+	ParseFormatYYYYMMDDWithHyphen = "2006-01-02"
+
+	// ParseFormatHHMM is for formatting time in the HH:MM format.
+	ParseFormatHHMM = "15:04"
+)
