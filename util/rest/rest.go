c69fcc6e3e3f53fb0e50e66b7fb03aa9c2c18c90
TH Huang <tzhsn.huang@gmail.com>
2024-03-22 23:25:55 -0500

    feat: add function for registering string tag for validation (#13)

--- util/rest/rest.go
+++ util/rest/rest.go
@@ -2,11 +2,24 @@ package rest
 
 import (
 	"reflect"
+	"regexp"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 )
 
+// RegisterStringTag registers a custom validation rule for a string field based on a regular expression.
+func RegisterStringTag(validate *validator.Validate, tag string, regex *regexp.Regexp) {
+	if err := validate.RegisterValidation(tag, func(fl validator.FieldLevel) bool {
+		s := fl.Field().String()
+		result := regex.MatchString(s)
+		return result
+	}); err != nil {
+		panic(err)
+	}
+}
+
+// ParseBody parses the request body into a struct and validates it.
 func ParseBody(c *fiber.Ctx, validate *validator.Validate, obj interface{}) error {
 	if err := c.BodyParser(obj); err != nil {
 		return err
